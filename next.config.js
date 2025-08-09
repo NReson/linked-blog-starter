bb40ce5bff05868dc1b7e527af67d50a526b4426
Ivan <notresonance@gmail.com>
2025-08-09 18:35:02 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,3 +9,18 @@ module.exports = {
     ]
   },
 }
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import remarkGfm from 'remark-gfm'
+import remarkImages from 'remark-images'
+
+export default makeSource({
+  contentDirPath: 'content',
+  documentTypes: [],
+  mdx: {
+    remarkPlugins: [
+      remarkGfm,
+      remarkImages, // без remark-copy-linked-files
+    ],
+    rehypePlugins: [],
+  }
+})
