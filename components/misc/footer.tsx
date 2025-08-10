5957109a9b84f8b3270d47b8806bf44b87fa1a2d
Ivan <notresonance@gmail.com>
2025-08-10 12:05:09 +0300

    Update footer.tsx

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <div className="container mx-auto px-5">
         <div className="py-1 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+          <h3 className="text-1xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             <PreviewLink href="">Statically Generated</PreviewLink> with Next.js.
           </h3>
         
