6b5f5aa52c23f6be00ef965061a7eeb266b553d0
Ivan <notresonance@gmail.com>
2025-08-10 08:56:32 +0300

    Create contentlayer.config.js

---
+++ contentlayer.config.js
@@ -0,0 +1,24 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import remarkGfm from 'remark-gfm'
+import remarkImages from 'remark-images'
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.md`,
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (doc) => `/${doc._raw.flattenedPath}`,
+    },
+  },
+}))
+
+export default makeSource({
+  contentDirPath: 'content', // папка с твоими md-файлами
+  documentTypes: [Post],
+  markdown: { remarkPlugins: [remarkGfm, remarkImages] },
+})
